4e526d5afabd5605a2e5a40beac8417b14bf47b4
WCEdmonds <wcedmonds28@gmail.com>
2015-09-20 15:01:04 -0400

    Fix

--- HiddenNumber.rb
+++ HiddenNumber.rb
@@ -1,11 +1,15 @@
+require 'pry'
+
+tries = 3
   random_number = rand(1..10)
+  binding.pry
 puts "What is your name?"
 name = gets.strip
 
 puts "Hello" + " " + name + "!"
 
 puts "Want to play a game, " + name + "?"
-answer = gets.strip
+answer = gets.strip.downcase
 
 if answer == "yes"
   print 'Great!'
@@ -15,13 +19,21 @@ if answer == "yes"
       print "Can you guess it? "
       guess = gets.to_i
 
+      if tries == 1
+        puts "Out of tries. You lose, #{name}. :("
+        break
+      end
       if guess < random_number
-          puts "My number's bigger than that. Try again."
+          tries -= 1
+          puts "My number's bigger than that. You have #{tries} left. Try again."
+
       elsif guess > random_number
-          puts "My number's smaller than that. Try again."
-      else
-        puts  "Wow! You got it!"
-        exit
+          tries -= 1
+          puts "My number's smaller than that. You have #{tries} left. Try again."
+      elsif guess == random_number
+        puts  "Wow! You got it, #{name}! :)"
+
+            break
+          end
       end
   end
-end
